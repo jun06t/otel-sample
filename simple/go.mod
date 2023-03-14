492a181f745036bd1fca7c16c3569a7379f69f1c
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2023-03-15 07:40:17 +0900

    feat: add simple pattern

---
+++ simple/go.mod
@@ -0,0 +1,20 @@
+module github.com/jun06t/otel-sample/simple
+
+go 1.20
+
+require (
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.40.0
+	go.opentelemetry.io/otel v1.14.0
+	go.opentelemetry.io/otel/exporters/jaeger v1.14.0
+	go.opentelemetry.io/otel/exporters/stdout/stdouttrace v1.14.0
+	go.opentelemetry.io/otel/sdk v1.14.0
+	go.opentelemetry.io/otel/trace v1.14.0
+)
+
+require (
+	github.com/felixge/httpsnoop v1.0.3 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/otel/metric v0.37.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
---
+++ simple/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+)
+
+var tracer trace.Tracer
+
+func main() {
+	backend := os.Getenv("BACKEND_ADDR")
+
+	_, cleanup, err := NewTracerProvider("otel-sample")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cleanup()
+	tracer = otel.Tracer("gateway")
+
+	h := newHandler(backend)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", http.HandlerFunc(h.alive))
+	mux.Handle("/hello", otelhttp.NewHandler(http.HandlerFunc(h.hello), "server"))
+	http.ListenAndServe(":8000", mux)
+}
+
+type handler struct {
+	cli http.Client
+}
+
+func newHandler(addr string) *handler {
+	hc := http.Client{
+		Transport: otelhttp.NewTransport(http.DefaultTransport),
+	}
+	return &handler{
+		cli: hc,
+	}
+}
+
+func (h *handler) alive(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Alive")
+}
+
+func (h *handler) hello(w http.ResponseWriter, r *http.Request) {
+	Operation1(r.Context())
+	Operation2(r.Context())
+
+	hreq, err := http.NewRequestWithContext(r.Context(), "GET", "http://httpbin.org/delay/1", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := h.cli.Do(hreq)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	Operation3(r.Context())
+}
+
+func Operation1(ctx context.Context) {
+	_, span := tracer.Start(ctx, "op1")
+	defer span.End()
+	time.Sleep(100 * time.Millisecond)
+}
+
+func Operation2(ctx context.Context) {
+	_, span := tracer.Start(ctx, "op2")
+	defer span.End()
+	time.Sleep(100 * time.Millisecond)
+}
+
+func Operation3(ctx context.Context) {
+	_, span := tracer.Start(ctx, "op3")
+	defer span.End()
+	time.Sleep(100 * time.Millisecond)
+}
